1fd04e3380abb9b8164e9b6fd8a5262bff064103
Benjamin Shen <bshen7225@gmail.com>
2024-06-11 21:12:31 -0400

    Final Lab 8.

--- lab8/src/CommonListADT.java
+++ lab8/src/CommonListADT.java
@@ -11,10 +11,10 @@ public interface CommonListADT<T> {
    * A general purpose map higher order function on this list, that returns
    * the corresponding list of type R.
    *
-   * @param converter    the function that converts T into R
-   * @param <R>    the type of data in the resulting list
-   * @return    the resulting list that is identical in structure to this list,
-   * but has data of type R
+   * @param converter the function that converts T into R
+   * @param <R>       the type of data in the resulting list
+   * @return          the resulting list that is identical in structure to this list,
+   *                  but has data of type R
    */
   <R> CommonListADT<R> map(Function<T, R> converter);
 
